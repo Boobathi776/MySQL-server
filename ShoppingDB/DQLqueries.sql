a00208d9f6a641169d3fcf8a028079962308d7eb
BOOBATHI A <146323729+Boobathi776@users.noreply.github.com>
2025-06-06 14:30:08 +0530

    Add files via upload

--- ShoppingDB/DQLqueries.sql
+++ ShoppingDB/DQLqueries.sql
@@ -13,3 +13,15 @@ join Orders o on  o.CustomerID = c.ID
 join Inventory inv on inv.ID = o.ProductID;
 
 select * from  vwCustomerWithProducts;
+
+drop view vwCustomerWithProducts;
+
+create view vwCustomerWithProducts
+as
+select c.Name as [Customer name],inv.ProductName,inv.Price,o.Quantity,o.TotalAmount
+from Customer c 
+left join Orders o on  o.CustomerID = c.ID
+left join Inventory inv on inv.ID = o.ProductID;
+
+
+select * from  vwCustomerWithProducts;
