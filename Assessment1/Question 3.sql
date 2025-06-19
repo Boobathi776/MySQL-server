856fa0304346a96ef4462281b0a807a4f1a811cf
BOOBATHI A <146323729+Boobathi776@users.noreply.github.com>
2025-06-19 11:13:21 +0530

    Correct answer

--- Assessment1/Question 3.sql
+++ Assessment1/Question 3.sql
@@ -11,6 +11,7 @@ OrderDate date
 insert into Orders (CustomerID, OrderDate) values (1,'2024-12-10'),(1, '2025-01-05'),(1, '2025-02-15'),(1, '2025-03-20'),(1, '2025-04-10'),(1, '2025-05-11'),
 (1, '2025-06-12'),(2, '2025-01-07'),(2, '2025-02-18'),(2, '2025-04-21'),(2, '2025-05-05'),(2, '2025-06-01'),(3, '2025-03-10'),(3, '2025-06-08');
 
+insert into Orders (CustomerID ,OrderDate) values(1,'2025-01-06')
 --solution 
 select CustomerID,Count(OrderDate) as OrderCount
 from Orders 
@@ -18,5 +19,20 @@ where OrderDate between dateadd(month,-6,getdate()) and cast(getdate() as date)
 group by CustomerID
 having count(OrderDate) = 6;
 
+
+--Correct solution 
+with CustomerOrderedMonths as(
+select CustomerID ,month(OrderDate) as MonthsOrdered,count(OrderDate) as [no of orders]
+from Orders
+where OrderDate between dateAdd(month,-6,getdate()) and cast(getdate() as date)
+group by month(orderDate),CustomerID
+)
+select CustomerID
+from CustomerOrderedMonths cm 
+group by CustomerID 
+having count(MonthsOrdered) = 6
+
+
+select * from Orders;
 
 
